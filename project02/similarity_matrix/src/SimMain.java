e1d1b93f68e36ea32dd79589b6282a1ec9908b60
Levente Sipek <levente.sipeki@gmail.com>
2011-09-14 20:22:30 -0600

    added comments/ double checked algorithms and fixed a problem

--- project02/similarity_matrix/src/SimMain.java
+++ project02/similarity_matrix/src/SimMain.java
@@ -2,8 +2,8 @@
 
 public class SimMain {
 	public static void main(String[] arg){
-		int[] a  = {10, 4, 5, 6, 7, 9};
-		int[] b  = {10, 4, 5, 6, 7, 9};
+		int[] a  = {10, 4, 5, 6, 7, 9 , -1};    // negative represents a missing attirbute in that area
+		int[] b  = {10, 4, 5, 6, 7, 9, 10};
 		int[] c  = {6, 4, 5, 1, 5, 1};
 		int[] bina = {1, 0, 1, 1, 0, 1, 0};
 		int[] binb = {1, 0, 1, 1, 0, 1, 0};
@@ -33,8 +33,8 @@ public class SimMain {
 		System.out.println("The SMC correlation between 1 and 2 is : " + alg.smc(bina, binb));
 		System.out.println("The SMC correlation between 1 and 3 is : " + alg.smc(bina, binc));
 		System.out.println(); 
-		System.out.println("The Jaccard coefficient between 1 and 2 is : " + alg.smc(bina, binb));
-		System.out.println("The Jaccard coefficient between 1 and 3 is : " + alg.smc(bina, binc));
+		System.out.println("The Jaccard coefficient between 1 and 2 is : " + alg.Jaccard(bina, binb));
+		System.out.println("The Jaccard coefficient between 1 and 3 is : " + alg.Jaccard(bina, binc));
 	}
 	public static void printarray(int[] a){
 		System.out.print("{");
--- project02/similarity_matrix/src/algorithms.java
+++ project02/similarity_matrix/src/algorithms.java
@@ -2,41 +2,41 @@ import java.util.ArrayList;
 
 
 public class algorithms {
-	public double euclidean(int[] a, int[] b){
+	public double euclidean(int[] a, int[] b){    // euclidean distnace 
 		double result =0; 
-		ArrayList<Integer> both = both_have(a, b);
+		ArrayList<Integer> both = both_have(a, b);   // this function finds the common areas of both parties 
 		double sum=0; 
 		for(int i=0; i<both.size(); i++){
-			double sub = a[both.get(i)] - b[both.get(i)]; 
-			sum = sum + Math.pow(sub, 2); 
+			double sub = a[both.get(i)] - b[both.get(i)];   // getting the difference between the two coensiding attributes 
+			sum = sum + Math.pow(sub, 2); 					// raising it to a power of 2 and adding it to the other sums 
 		}
-		result  = Math.sqrt(sum);
-		return 1/(1 + result);
+		result  = Math.sqrt(sum);						// taking the square root of the sums 
+		return 1/(1 + result);						// normalizing the number 
 		
 	}
 	public double smc(int[] a, int[] b){
-		ArrayList<Integer> both = both_have(a, b); 
-		int matching_attributes = 0 ; 
-		for( int i =0; i<both.size(); i++){
-			if (a[both.get(i)] == b[both.get(i)]){
+		ArrayList<Integer> both = both_have(a, b);    // finding the attributes that they both have 
+		int matching_attributes = 0 ;   			// initializing attributes that they both have in common
+		for( int i =0; i<both.size(); i++){				
+			if (a[both.get(i)] == b[both.get(i)]){			// goes through array and finds matching attribtues 
 				matching_attributes ++;
 			}
 		}
-		double attributes = both.size(); 
+		double attributes = both.size(); 			// divide the matching attirbues by the size 
 		return matching_attributes/attributes;
 	}
 	
 	public double Jaccard(int[] a, int[] b){
-		ArrayList<Integer> both = both_have(a, b); 
+		ArrayList<Integer> both = both_have(a, b); 			// finding the attributes that they both have 
 		int matching_attributes = 0 ; 
 		for( int i =0; i<both.size(); i++){
-			if (a[both.get(i)] == b[both.get(i)] && a[both.get(i)] > 0 ){
+			if (a[both.get(i)] == b[both.get(i)] && a[both.get(i)] > 0 ){    // finding all matching possitive attribues 
 				matching_attributes ++;
 			}
 		}
-		double attributes = 0;
+		double attributes = 0;							// finds all attribues 
 		for( int i=0; i<both.size(); i++){
-			if(a[both.get(i)] != 0)
+			if(a[both.get(i)] != 0)					// finds all attributes with a value on either side that is present 
 				attributes++;
 			else if(b[both.get(i)] != 0)
 				attributes++; 
@@ -44,31 +44,31 @@ public class algorithms {
 		return matching_attributes/attributes;	
 	}
 	public double Pearson(int[] a, int[] b){
-		ArrayList<Integer> both = both_have(a, b); 
-		double suma = sum(a, both);
-		double sumb = sum(b, both); 
-		double sumasq = sumsq(a, both); 
-		double sumbsq = sumsq(b, both);
-		double pSum = sumprod(a, b, both);
+		ArrayList<Integer> both = both_have(a, b); // finding the attributes that they both have 
+		double suma = sum(a, both);					// sum of a attributes 
+		double sumb = sum(b, both); 				// sum of b attributes 
+		double sumasq = sumsq(a, both); 			// the sum of squares of a's attributes 
+		double sumbsq = sumsq(b, both);				// the sum of squares of b's attributes
+		double pSum = sumprod(a, b, both);			// sum of the product of the two data arrays 
 		
-		double num = pSum - (suma * sumb/both.size()); 
-		double den = Math.sqrt((sumasq - Math.pow(suma, 2)/both.size()) * (sumbsq - Math.pow(sumb, 2)/both.size())); 
+		double num = pSum - (suma * sumb/both.size());         // takes the product sum and divides it by the multiple of the sums divided by the size 
+		double den = Math.sqrt((sumasq - Math.pow(suma, 2)/both.size()) * (sumbsq - Math.pow(sumb, 2)/both.size()));  // takes the square root fo the squares of the sums mius the sums sqared and devided by size 
 		
-		if (den == 0 )
+		if (den == 0 )      // returns 0 if the den is 0 
 			return 0 ; 
 		else
-			return num/den;
+			return num/den;     // returns Pearson Correlation 
 		
 	}
 	public double Cosine(int[] a, int[] b){
 		double result =0; 
-		ArrayList<Integer> both = both_have(a, b); 
-		double dot = dotproduct(a, b, both); 
-		result = dot / (mag(a, both) * mag(b, both)); 
-		return result;
+		ArrayList<Integer> both = both_have(a, b); 		// finding the attributes that they both have
+		double dot = dotproduct(a, b, both); 			// finds the dot product of the two arrays 
+		result = dot / (mag(a, both) * mag(b, both)); 		// divides dot product by the magnitude a times magnitude b
+		return result;					
 	}
 	
-	public ArrayList<Integer> both_have(int a[], int[] b){
+	public ArrayList<Integer> both_have(int a[], int[] b){      // this function finds all the values in which both arrays have a number that's not negative 
 		ArrayList<Integer> both = new ArrayList<Integer>(); 
 		for(int i=0; i<a.length; i++){
 			if(a[i] >= 0 && b[i] >= 0){
@@ -77,7 +77,7 @@ public class algorithms {
 		}
 		return both; 
 	}
-	public double sum (int[] a, ArrayList<Integer> x){
+	public double sum (int[] a, ArrayList<Integer> x){			// reuturns the sum of all vlaues in the array 
 		double sum = 0 ;
 		for (int i=0; i<x.size(); i++){
 			sum = sum + a[x.get(i)]; 
@@ -85,7 +85,7 @@ public class algorithms {
 	
 		return sum; 
 	}
-	public double sumsq (int[] a, ArrayList<Integer> x){
+	public double sumsq (int[] a, ArrayList<Integer> x){		// returns the sum of all squared attributes of a data array 
 		double sum = 0 ;
 		for (int i=0; i<x.size(); i++){
 			sum = sum + Math.pow(a[x.get(i)],2 ); 
@@ -93,7 +93,7 @@ public class algorithms {
 	
 		return sum; 
 	}
-	public double sumprod (int[] a, int[] b, ArrayList<Integer> x){
+	public double sumprod (int[] a, int[] b, ArrayList<Integer> x){			// returns the sum of all vlaues of a and b sequentially multiplied togeter (a1*b1 + a2*b2  ... ) 
 		double sum = 0 ;
 		for (int i=0; i<x.size(); i++){
 			sum = sum + (a[x.get(i)] * b[x.get(i)]);  
@@ -101,7 +101,7 @@ public class algorithms {
 	
 		return sum; 
 	}
-	public double dotproduct(int[] a, int[] b, ArrayList<Integer> x){
+	public double dotproduct(int[] a, int[] b, ArrayList<Integer> x){     // returns the dot product of two arrays 
 		double result = 0;
 		for(int i=0; i<x.size(); i++){
 			result = result + (a[x.get(i)] * b[x.get(i)]);
@@ -109,7 +109,7 @@ public class algorithms {
 		
 		return result; 
 	}
-	public double mag(int[] a, ArrayList<Integer> x){
+	public double mag(int[] a, ArrayList<Integer> x){   			// returns the magnitude of the array 
 		double result = 0; 
 		for(int i=0; i<x.size(); i++){
 			result = result + (a[x.get(i)] * a[x.get(i)]);
